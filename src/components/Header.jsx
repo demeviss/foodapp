154330488b9c1abc4c92635ab2bc7c4587682421
diana <demvi16@gamil.com>
2023-12-21 12:12:30 -0600

    name css
    name css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ export const Header = () =>{
     return(
         <nav id="header"className="bg-black text-white" >
             <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2" >
-                <div className="logo-wrapper">
+                <div className="logo-wrapper pl-4 flex items-center">
                     <img src= {"/"} alt="logo"/>
                 </div>
-                <div className="nav-menu-wrapper">
+                <div className="nav-menu-wrapper flex items-center justify-between space-x-10">
                     <div>Home</div>
                     <div>About</div>
                 </div>
